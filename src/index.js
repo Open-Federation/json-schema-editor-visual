b4951d2a623ff9367f0610bcb978bca4d67502f9
suxiaoxin <hellosean1025@163.com>
2018-03-15 17:43:19 +0800

    feat: css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import {combineReducers, createStore , applyMiddleware} from 'redux'
+import 'antd/dist/antd.css'
 import jEditor from '../package/index.js'
 
 const store = jEditor.Model.getStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
