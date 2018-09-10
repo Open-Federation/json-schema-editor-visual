00e3b9424bda0be0d54b8c1cd94ec6eadb19eb02
gaoxiaolin.gao <gaoxiaolin.gao@qunar.com>
2018-09-10 20:00:33 +0800

    feat:  新增节点默认为requred

--- package/models/schema.js
+++ package/models/schema.js
@@ -146,27 +146,49 @@ export default {
     let name = action.name;
     let propertiesData = utils.getData(oldData, keys);
     let newPropertiesData = {};
+
+    let parentKeys = utils.getParentKeys(keys);
+    let parentData = utils.getData(oldData, parentKeys);
+    let requiredData = [].concat(parentData.required || []);
+
     if (!name) {
       newPropertiesData = Object.assign({}, propertiesData);
-      newPropertiesData['field_' + fieldNum++] = utils.defaultSchema.string;
+      let ranName = 'field_' + fieldNum++;
+      newPropertiesData[ranName] = utils.defaultSchema.string;
+      requiredData.push(ranName);
     } else {
       for (let i in propertiesData) {
         newPropertiesData[i] = propertiesData[i];
         if (i === name) {
-          newPropertiesData['field_' + fieldNum++] = utils.defaultSchema.string;
+          let ranName = 'field_' + fieldNum++;
+          newPropertiesData[ranName] = utils.defaultSchema.string;
+          requiredData.push(ranName);
         }
       }
     }
     utils.setData(state.data, keys, newPropertiesData);
+    // add required
+    parentKeys.push('required');
+    utils.setData(state.data, parentKeys, requiredData);
   },
   addChildFieldAction: function(state, action, oldState) {
     const keys = action.key;
     let oldData = oldState.data;
     let propertiesData = utils.getData(oldData, keys);
     let newPropertiesData = {};
+
     newPropertiesData = Object.assign({}, propertiesData);
-    newPropertiesData['field_' + fieldNum++] = utils.defaultSchema.string;
+    let ranName = 'field_' + fieldNum++;
+    newPropertiesData[ranName] = utils.defaultSchema.string;
     utils.setData(state.data, keys, newPropertiesData);
+
+    // add required
+    let parentKeys = utils.getParentKeys(keys);
+    let parentData = utils.getData(oldData, parentKeys);
+    let requiredData = [].concat(parentData.required || []);
+    requiredData.push(ranName);
+    parentKeys.push('required');
+    utils.setData(state.data, parentKeys, requiredData);
   },
 
   setOpenValueAction: function(state, action, oldState) {
